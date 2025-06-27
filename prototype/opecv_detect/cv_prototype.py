b06d9e862ecb1d85dcb4cf25ec556b53416730ed
clear-head <134698515+Clear-head@users.noreply.github.com>
2025-06-27 12:04:36 +0900

    Update cv_prototype.py

--- prototype/opecv_detect/cv_prototype.py
+++ prototype/opecv_detect/cv_prototype.py
@@ -17,8 +17,6 @@ while(cap.isOpened()):
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        roi_gray = gray[y : y + h, x : x + w]
-        roi_color = frame[y : y + h, x : x + w]
 
     cv2.imshow('frame', frame)
 
